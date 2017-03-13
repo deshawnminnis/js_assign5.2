303812e9a1c84be00e3b1c624e4222fa08095061
De'Shawn Minnis <deshawnminnis@gmail.com>
2017-03-13 16:40:19 -0700

    Add files via upload

---
+++ Name_Dob_Age.js
@@ -0,0 +1,12 @@
+
+       // Output Name, DOB and Place of birth Function//
+            var name = "De'Shawn Minnis";
+            var DOB = "12/18/1979";
+            var age = 37;
+            var placOfBirth = "Seattle WA" ;
+                
+
+            console.log(name)
+            console.log(DOB)
+            console.log(age)
+            console.log(placOfBirth)
